1c2e3c464f04787f5ccbc28818bef4a06a86041b
Joash <guanzonjoashmatthew@gmail.com>
2024-03-23 23:35:33 +0800

    Added Print Statements

--- ask_fancy_name_job_favorite.py
+++ ask_fancy_name_job_favorite.py
@@ -14,5 +14,13 @@ DreamJob = input("Please Enter Your Dream Job: ")
 FaveColor = input("What is your Favorite Colour?: ")
 
 #Print Fancy Name
+FancyName = pyfiglet.figlet_format(name, font='isometric1')
+print(Colorate.Horizontal(Colors.rainbow, FancyName))
+
 #Print Fancy Job
+FancyJobName = pyfiglet.figlet_format(DreamJob, font='isometric1')
+print(Colorate.Horizontal(Colors.rainbow, FancyJobName))
+
 #Print Fancy Fave Color
+FancyFaveColor = pyfiglet.figlet_format(FaveColor, font='isometric1')
+print(Colorate.Horizontal(Colors.rainbow, FancyFaveColor))
