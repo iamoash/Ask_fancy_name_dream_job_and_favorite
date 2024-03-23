75763979d282d87c0f1e95c6d1df2ade704083a0
Joash <guanzonjoashmatthew@gmail.com>
2024-03-23 23:31:01 +0800

    Added Input Statements

--- ask_fancy_name_job_favorite.py
+++ ask_fancy_name_job_favorite.py
@@ -5,8 +5,13 @@ from pystyle import Colors, Colorate
 #PSEUDOCODE
 
 #Input Name
+name = input("Please Enter Your Name: ")
+
 #Input Dream Job
+DreamJob = input("Please Enter Your Dream Job: ")
+
 #Input Favorite Color
+FaveColor = input("What is your Favorite Colour?: ")
 
 #Print Fancy Name
 #Print Fancy Job
